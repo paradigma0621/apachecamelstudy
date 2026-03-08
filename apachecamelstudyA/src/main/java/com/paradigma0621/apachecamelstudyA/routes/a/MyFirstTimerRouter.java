6c41d92bf1e266d29dc9d61c172d175a4e99d9df
Lucas Favaro <paradigma0621@gmail.com>
2026-03-08 18:44:46 -0300

    Now correctly updates and displays the current time using bean

--- apachecamelstudyA/src/main/java/com/paradigma0621/apachecamelstudyA/routes/a/MyFirstTimerRouter.java
+++ apachecamelstudyA/src/main/java/com/paradigma0621/apachecamelstudyA/routes/a/MyFirstTimerRouter.java
@@ -12,8 +12,17 @@ public class MyFirstTimerRouter extends RouteBuilder{
 	public void configure() throws Exception {
 		from("timer:first-timer")
 		// .transform().constant("My constant message") // Prints constant message in console log
-		.transform().constant("Time now is: " + LocalDateTime.now()) // Prints the same time message every time
+		//.transform().constant("Time now is: " + LocalDateTime.now()) // Prints the same time message every time
+		.bean("getCurrentTimeBean") // the bean name must start with a lowercase letter
+									// now correctly updates and displays the current time
 		.to("log:first-timer");
 	}
 
+}
+
+@Component
+class GetCurrentTimeBean {
+	public String getCurrentTime() {
+		return "Time now is: " + LocalDateTime.now();
+	}
 }
