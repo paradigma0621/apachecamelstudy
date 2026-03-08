96498966a06c3343f3cd3a593883c88d2f7901ff
Lucas Favaro <paradigma0621@gmail.com>
2026-03-08 18:37:16 -0300

    Modify the message displayed in the console

--- apachecamelstudyA/src/main/java/com/paradigma0621/apachecamelstudyA/routes/a/MyFirstTimerRouter.java
+++ apachecamelstudyA/src/main/java/com/paradigma0621/apachecamelstudyA/routes/a/MyFirstTimerRouter.java
@@ -3,13 +3,17 @@ package com.paradigma0621.apachecamelstudyA.routes.a;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class MyFirstTimerRouter extends RouteBuilder{
 	
 	@Override
 	public void configure() throws Exception {
 		from("timer:first-timer")
-		.to("log:first-timer");  // Prints null message in console log
+		// .transform().constant("My constant message") // Prints constant message in console log
+		.transform().constant("Time now is: " + LocalDateTime.now()) // Prints the same time message every time
+		.to("log:first-timer");
 	}
 
 }
