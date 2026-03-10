846e5824805c008618ea72941bcd984cce4838e5
Lucas Favaro <paradigma0621@gmail.com>
2026-03-10 19:01:24 -0300

    Convert CurrencyExchange record to Lombok class. Add Processor and Transformer for JSON file transferred from Microservice A to Microservice B

--- apachecamelstudyB/src/main/java/com/paradigma0621/apachecamelstudyB/model/CurrencyExchange.java
+++ apachecamelstudyB/src/main/java/com/paradigma0621/apachecamelstudyB/model/CurrencyExchange.java
@@ -1,5 +1,17 @@
 package com.paradigma0621.apachecamelstudyB.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
-public record CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) { }
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyExchange {
+    Long id;
+    String from;
+    String to;
+    BigDecimal conversionMultiple;
+}
--- apachecamelstudyB/src/main/java/com/paradigma0621/apachecamelstudyB/routes/ActiveMqReceiverRouter.java
+++ apachecamelstudyB/src/main/java/com/paradigma0621/apachecamelstudyB/routes/ActiveMqReceiverRouter.java
@@ -2,13 +2,19 @@ package com.paradigma0621.apachecamelstudyB.routes;
 
 import com.paradigma0621.apachecamelstudyB.model.CurrencyExchange;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 @RequiredArgsConstructor
 @Component
 public class ActiveMqReceiverRouter extends RouteBuilder {
+
+    private final MyCurrencyExchangeProcessor myCurrencyExchangeProcessor;
+    private final MyCurrencyExchangeTransformer myCurrencyExchangeTransformer;
 
     @Override
     public void configure() throws Exception {
@@ -16,7 +22,35 @@ public class ActiveMqReceiverRouter extends RouteBuilder {
         //.to("log:received-message-from-active-mq");
 
         from("activemq:my-activemq-queue")
-        .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+        .unmarshal()
+        .json(JsonLibrary.Jackson, CurrencyExchange.class)
+		.bean(myCurrencyExchangeProcessor)
+		.bean(myCurrencyExchangeTransformer)
         .to("log:received-message-from-active-mq");
+    }
+}
+
+
+@Slf4j
+@Component
+class MyCurrencyExchangeProcessor {
+
+    public void processMessage(CurrencyExchange currencyExchange) {
+
+        log.info("Do some processing wiht currencyExchange.getConversionMultiple() value which is {}",
+                currencyExchange.getConversionMultiple());
+
+    }
+}
+
+@Component
+class MyCurrencyExchangeTransformer {
+
+    public CurrencyExchange processMessage(CurrencyExchange currencyExchange) {
+
+        currencyExchange.setConversionMultiple(currencyExchange.getConversionMultiple().multiply(BigDecimal.TEN));
+
+        return currencyExchange;
+
     }
 }
